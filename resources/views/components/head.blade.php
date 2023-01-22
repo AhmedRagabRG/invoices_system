581691551f79752ac0e365a2a6af0af5db33dda0
ahmedragabrg <ahmedragabrg>
2023-01-22 12:16:13 +0200

    initil commit

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,14 +1,14 @@
 <!-- BOOTSTRAP CSS -->
-<link id="style" href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+<link id="style" href="{{asset('assets/plugins/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" />
 
 <!-- STYLE CSS -->
-<link href="assets/css/style.css" rel="stylesheet" />
-<link href="assets/css/dark-style.css" rel="stylesheet" />
-<link href="assets/css/transparent-style.css" rel="stylesheet">
-<link href="assets/css/skin-modes.css" rel="stylesheet" />
+<link href="{{asset('assets/css/style.css')}}" rel="stylesheet" />
+<link href="{{asset('assets/css/dark-style.css')}}" rel="stylesheet" />
+<link href="{{asset('assets/css/transparent-style.css')}}" rel="stylesheet">
+<link href="{{asset('assets/css/skin-modes.css')}}" rel="stylesheet" />
 
 <!--- FONT-ICONS CSS -->
-<link href="assets/css/icons.css" rel="stylesheet" />
+<link href="{{asset('assets/css/icons.css')}}" rel="stylesheet" />
 
 <!-- COLOR SKIN CSS -->
-<link id="theme" rel="stylesheet" type="text/css" media="all" href="assets/colors/color1.css" />
+<link id="theme" rel="stylesheet" type="text/css" media="all" href="{{asset('assets/colors/color1.css')}}" />
